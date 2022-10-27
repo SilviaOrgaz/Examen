c011d993b42f2a652259d918be92e792e00a6ec5
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-27 20:33:49 +0200

    he creado la funcion imprimir

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -3,3 +3,10 @@ lista = [18, 50, 210, 80, 145, 333, 70, 30]
 #Organiza la lista mediante el metodo de ordenamiento sort
 lista.sort()
 print(lista)
+
+def imprimir_numero(lista):
+    for i in lista:
+        if i % 10 == 0 and i < 200:
+            print (i)
+
+imprimir_numero(lista)
