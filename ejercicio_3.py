ce04a7c6354d15042f8944a70efec1c2172b8cdf
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-27 19:08:04 +0200

    he creado el str

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -2,5 +2,7 @@ class Alumno:
     def __init__(self, nombre, nota):
         self.nombre = nombre
         self.nota = nota
-        
+
+    def __str__(self):
+        return "El nombre del alumno es {} y su nota es de {}".format(self.nombre, self.nota)
         
