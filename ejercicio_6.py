8cb270038e034ceb9dc5b0f916b4772d97865fd6
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-27 20:50:38 +0200

    he creado la otra forma

--- ejercicio_6.py
+++ ejercicio_6.py
@@ -1,8 +1,15 @@
 #Creacion de una matriz como lista de lista
+import numpy as np
+x =np.zeros(5,5)
+print(x)
+x += np.arange(5)
+print(x)
+
 def crear_matriz(numero_filas, numero_columnas):
     matriz = [None] * numero_filas
     for i in range(numero_filas):
         matriz[i] = [None] * numero_columnas
+    
 
 def dibujar_matriz(m3):
     print("La matriz es: ")
@@ -11,3 +18,4 @@ def dibujar_matriz(m3):
 
 matriz = [[1,2,4],[2,3,5],[8,5,9],[1,2,3], [4,5,6]]
 dibujar_matriz(matriz)
+
