64fac22393254207151db1d7ff480631b3a7d663
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-27 19:56:45 +0200

    he creado clase datopolinomio

--- ejercicio_7.py
+++ ejercicio_7.py
@@ -1,2 +1,10 @@
+from matplotlib.rcsetup import validate_int_or_None
+
+
 class Nodo(object):
     info, sig = None, None
+
+class datoPolinomio(object):
+    def __init__(self, valor, termino):
+        self.valor = valor
+        self.termino = termino
