d0b1d474f2b69114ee8f63bfafa34d7eb390e670
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-27 19:26:52 +0200

    he creado la clase imprime_tabla

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -1,5 +1,10 @@
 class hash_tabla:
+    #En primer lugar creo un constructor donde se crea la tabla hash de un determinado tamaño: tamaño_tab
     def __init__(self, tamaño_tab):
         self.tamaño_tab = tamaño_tab
         self.tabla =  [None] * tamaño_tab
+
+    def imprime_tabla(self):
+        for i in range (len(self.tabla)):
+            
         
