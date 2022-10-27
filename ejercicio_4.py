28f986d552afd7f5b1d153460b2211651df9e6f0
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-27 19:16:57 +0200

    he copiado el ejercicio 3 para seguir haciendolo

---
+++ ejercicio_4.py
@@ -0,0 +1,16 @@
+class Alumno:
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+        print("Se ha creado con exito")
+
+    def calificacion(self):
+        if self.nota <= 5:
+            print("El alumno {} ha suspendido con una nota de {}".format(self.nombre, self.nota))
+        else:
+            print("El alumno {} ha aprobado con una nota de {}".format(self.nombre, self.nota))
+
+A = Alumno("Silvia", 4)
+B = Alumno("Cristina", 9)
+A.calificacion()
+B.calificacion()
