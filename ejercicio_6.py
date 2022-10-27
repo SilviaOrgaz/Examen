18d497003132ac4d4e38ef941e6cf505eae2735d
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-27 20:27:21 +0200

    he dibujado la matriz

--- ejercicio_6.py
+++ ejercicio_6.py
@@ -3,3 +3,11 @@ def crear_matriz(numero_filas, numero_columnas):
     matriz = [None] * numero_filas
     for i in range(numero_filas):
         matriz[i] = [None] * numero_columnas
+
+def dibujar_matriz(m3):
+    print("La matriz es: ")
+    for i in range(len(m3)):
+        print(m3[i])
+
+matriz = [[1,2,4],[2,3,5],[8,5,9],[1,2,3], [4,5,6]]
+dibujar_matriz(matriz)
