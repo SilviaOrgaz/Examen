addcf52a8c4b275329bea97259dbba2d1d2b017a
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-27 19:16:11 +0200

    he modificado el ejercicio

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -2,9 +2,7 @@ class Alumno:
     def __init__(self, nombre, nota):
         self.nombre = nombre
         self.nota = nota
-
-    def __str__(self):
-        return "El nombre del alumno es {} y su nota es de {}".format(self.nombre, self.nota)
+        print("Se ha creado con exito")
 
     def calificacion(self):
         if self.nota <= 5:
@@ -14,8 +12,6 @@ class Alumno:
 
 A = Alumno("Silvia", 4)
 B = Alumno("Cristina", 9)
-print(A)
-print(B)
 A.calificacion()
 B.calificacion()
 
